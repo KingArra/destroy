cc2192269c9e03f663ef0b64db7a3515830f79ac
Glitch (destroytheattacker) <none>
2020-08-11 00:15:06 +0000

    Rewound to commit b53294d0801992badf691ba2389e30a2bc33795e + 0 edits

--- server.js
+++ server.js
@@ -4596,7 +4596,7 @@ var maintainloop = (() => {
                         begin = 'A visitor is coming.';
                         arrival = names[0] + ' has arrived.'; 
                     } else {
-                        begin = 'The Next Wave will Start in 8 Seconds after this Wave.';
+                        begin = 'The 8 Second Cool Down has Ended.';
                         arrival = '';
                         arrival += 'Wave ' + wave + ' has Started.';
                     }
